457c0982c782a7c0945e9a608454262c7f6601e9
Vashu Singh <rajputvashusingh@gmail.com>
2025-04-11 15:36:51 +0530

    feat: initial project setup with Express.js backend
    - Add basic project structure with src directory
    - Implement Express.js server setup
    - Add MongoDB connection configuration
    - Set up utility files (asyncHandler, ApiResponse, apiError)
    - Configure environment variables and dependencies
    - Add git configuration files (.gitignore, .prettierrc)

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+import app from "./app.js"
+
+dotenv.config()
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })  
+})
+.catch((error) => {
+    console.log("MongoDB connection failed", error)
+    process.exit(1)
+})
---
+++ src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //Cloudinary URL
+            required: true
+        },
+        coverImage: {
+            type: String, //Cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"        
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10)
+    }
+    next()
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        email: this.email,
+        fullName: this.fullName,
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRE })
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRE })
+}
+
+export const user  = mongoose.model("User", userSchema)

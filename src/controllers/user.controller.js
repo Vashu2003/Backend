7076ef6d0ed8628eec129578be9aa8b9bb3c4356
Vashu Singh <rajputvashusingh@gmail.com>
2025-04-14 19:09:56 +0530

    fix: ensure user account details are updated correctly by awaiting the update operation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -221,7 +221,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
@@ -300,4 +300,4 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage
-};
+}; 
